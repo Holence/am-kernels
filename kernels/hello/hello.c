f8de7908acbb3a5380ecaac3e4016a512ef262ca
Holence <Holence08@gmail.com>
2024-08-22 19:09:04 +0800

    hello

--- kernels/hello/hello.c
+++ kernels/hello/hello.c
@@ -1,13 +1,16 @@
 #include <am.h>
 #include <klib-macros.h>
+#include <stdio.h>
 
 int main(const char *args) {
   const char *fmt =
-    "Hello, AbstractMachine!\n"
-    "mainargs = '%'.\n";
+      "Hello, AbstractMachine!\n"
+      "mainargs = '%'.\n";
 
   for (const char *p = fmt; *p; p++) {
     (*p == '%') ? putstr(args) : putch(*p);
   }
+
+  printf("%d %s %s\n", 114514, fmt, args);
   return 0;
 }
